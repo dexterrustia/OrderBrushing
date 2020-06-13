86bd683064f33063892e2fcc8c9679ad9410c0ea
Dexter Rustia <Dexter.Rustia@dynatacorp.com>
2020-06-13 04:21:22 -0600

    added nested loops for order brushing

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const orders = require('./orders');
+const ord = require('./ordersFinal');
+//const ord = require('./orders');
 const express = require('express');
 const app = express();
 
 
 app.get('/',(req,res) => {
     //res.send('Hellow word!') 
-    //console.log(orders)
-    const rowLen = 222750;//orders.length;
+    const orders = ord.orders;
+    // console.log(orders)
+    const rowLen = orders.length;
     //console.log(rowLen)
     let orderBrushing = [];
     let doneShops = [];
@@ -15,20 +17,29 @@ app.get('/',(req,res) => {
         const currentShop = orders[i].shopid; 
         const user = orders[i].userid;
         const time = orders[i].event_time.split(' ')[1].split(':')[0];
-        console.log(`shop: ${currentShop} :: time: ${time}`)
-        for(let j = i+1; j<rowLen; i++){
-            if(currentShop == orders[j].shopid){
-                if(time == orders[j].event_time){ 
-                    orderBrushing.push({
-                       shopid: orders[j].shopid,
-                       userid: orders[j].userid
-                    });
-                    doneShops.push(orders[j])
+        const date = orders[i].event_time.split(' ')[0]
+        console.log(`shop: ${currentShop} :: time: ${time}`);
+        if( !doneShops.includes(currentShop) ){
+            for(let j = i+1; j<rowLen; j++){ 
+                console.log(`${j}. => orders[j].shopid ${orders[j].shopid}`) 
+                const curTime = orders[j].event_time.split(' ')[1].split(':')[0];
+                const curDate = orders[j].event_time.split(' ')[0];
+                if(currentShop == orders[j].shopid){
+                    if(time == curTime && date == curDate){ 
+                        orderBrushing.push({
+                           shopid: orders[j].shopid,
+                           userid: orders[j].userid
+                        });
+                    }
                 }
+                
+                doneShops.push(orders[j].shopid);
             }
-        }
+        } 
     }
-
+    console.log('doneshop : '+doneShops)
+    console.log('orderBrushing'+orderBrushing)
+    console.log('rowLen'+rowLen)
     //return result; //JavaScript object
     res.send(orderBrushing); //JSON
         
