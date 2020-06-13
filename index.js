35757803c4951253fa5a8662a23832b6eb787b45
Dexter Rustia <Dexter.Rustia@dynatacorp.com>
2020-06-13 02:38:40 -0600

    updated loop

--- index.js
+++ index.js
@@ -5,13 +5,32 @@ const app = express();
 
 app.get('/',(req,res) => {
     //res.send('Hellow word!') 
-    const rowLen = orders.length;
+    //console.log(orders)
+    const rowLen = 222750;//orders.length;
+    //console.log(rowLen)
+    let orderBrushing = [];
+    let doneShops = [];
     for(let i = 0; i<rowLen; i++){
-
+        console.log(orders[1])
+        const currentShop = orders[i].shopid; 
+        const user = orders[i].userid;
+        const time = orders[i].event_time.split(' ')[1].split(':')[0];
+        console.log(`shop: ${currentShop} :: time: ${time}`)
+        for(let j = i+1; j<rowLen; i++){
+            if(currentShop == orders[j].shopid){
+                if(time == orders[j].event_time){ 
+                    orderBrushing.push({
+                       shopid: orders[j].shopid,
+                       userid: orders[j].userid
+                    });
+                    doneShops.push(orders[j])
+                }
+            }
+        }
     }
 
     //return result; //JavaScript object
-    res.send(orders); //JSON
+    res.send(orderBrushing); //JSON
         
 
 })
